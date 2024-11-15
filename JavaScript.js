d00edac5aeaf2edc281e54e2c550e2a381f5f4a9
Apichet-ap <65001308108@udru.ac.th>
2024-11-15 16:28:41 +0700

    Add files via upload

--- JavaScript.js
+++ JavaScript.js
@@ -1,3 +1,7 @@
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 const reviews = [
     { text: "สินค้าดีมาก! สัตว์เลี้ยงของฉันชอบมากค่ะ", author: "คุณสมศรี" },
     { text: "บริการรวดเร็วและเป็นกันเองมากครับ", author: "คุณสมชาย" },
@@ -5,7 +9,7 @@ const reviews = [
     { text: "ราคาคุ้มค่า บริการดีเยี่ยม", author: "คุณสมบัติ" },
     { text: "แนะนำเลยค่ะ สินค้าดีจริง", author: "คุณสมใจ" },
     { text: "พนักงานให้คำแนะนำดีมาก", author: "คุณสมหมาย" },
-    { text: "สินค้าตรงตามที่โฆษณาไว้", author: "คุณสมศักดิ์" },
+    { text: "สินค้าตรงตามที่โฆษณาไว้", author: "คณสมศักดิ์" },
     { text: "จะกลับมาซื้ออีกแน่นอน", author: "คุณสมพร" }
     // เพิ่มรีวิวเพิ่มเติมตามต้องการ
 ];
